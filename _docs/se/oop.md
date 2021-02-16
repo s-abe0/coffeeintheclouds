0abcfdc3f06d6500eaec874504d06d805e3f7271
s-abe0 <sabe@intellective.com>
2021-02-16 15:10:44 -0500

    update sidenav and url config

---
+++ _docs/se/oop.md
@@ -0,0 +1,5 @@
+---
+layout: default
+---
+
+## Object Oriented Principles
---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+    $("#side-nav").navgoco({
+        caretHtml: '',
+        accordion: true,
+        openClass: 'open',
+        save: true,
+        slide: {
+            duration: 400,
+            easing: 'swing'
+        }   
+    });
+});
